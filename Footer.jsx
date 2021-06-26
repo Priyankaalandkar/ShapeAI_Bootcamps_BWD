8ddef12e677e67ffa20a403dfc0ebd29cbd340e2
Priyankaalandkar <86522703+Priyankaalandkar@users.noreply.github.com>
2021-06-26 20:23:51 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <footer>
+      <p> Copyright by ShapeAI @ {new Date().getFullYear()}</p>
+    </footer>
+  );
+}
+
+export default Header;
